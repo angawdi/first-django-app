de5c8a2576c6c0fff8468b1f91eddb20e3228ca4
Sky <anvandp18@gmail.com>
2018-09-27 10:39:22 -0700

    just run the server

--- django/first_django_app/first_django_app/settings.py
+++ django/first_django_app/first_django_app/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'first_django_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'djangotest'
+        'NAME': 'djangotest',
         'HOST': 'localhost'
     }
 }
--- django/first_django_app/test_app/views.py
+++ django/first_django_app/test_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	
+	print('hello from the index route')
+	return HttpResponse('This is kind of like a res.send in express')
