3eda93d31a86223c267baf29061e572c3da49b20
Sky <anvandp18@gmail.com>
2018-09-27 12:19:34 -0700

    working with the admin

--- django/first_django_app/test_app/models.py
+++ django/first_django_app/test_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+	title = models.CharField(max_length=200)
+	pages = models.IntegerField()
+	author = models.CharField(max_length=100)
+	award_winning = models.BooleanField(default=False)
+
+	def __str__(self):
+		return 'Bob\'s book: ' + self.title + ' has ' + str(self.pages) + ' pages' 
--- django/first_django_app/test_app/views.py
+++ django/first_django_app/test_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
 def index(request):
@@ -8,11 +9,13 @@ def index(request):
 
 def bob(request):
 	print('Bob says hi')
+	bobs_books = Book.objects.all().order_by('title')
 	return render(request,
 		'test_app/bob.html', 
 		{ 
 
 			'bobs_fave_food': 'Beans', 
 			'bob_is_tired': True,
-			'bobs_pets': ['dog', 'dog', 'cat', 'bird']
+			'bobs_pets': ['dog', 'dog', 'cat', 'bird'],
+			'bobs_books': bobs_books
 		})
